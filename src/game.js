c97c2e10204de8da752392c9ced655ead839a548
PARK-CHANUK <doosan1224@naver.com>
2023-09-05 18:51:20 +0900

    refactor: 모든 함수기능들을 클래스 메서드로 변경
    클래스 모듈화를 하기 위해서 모든 함수를 클래스 메서드로 변경함

---
+++ src/game.js
@@ -0,0 +1,101 @@
+export default class BaseballGame {
+  constructor() {
+    this.isStart = true;
+    this.computerInputNumbers = 0;
+    this.form = document.querySelector('form');
+    this.result = document.getElementById('result');
+    this.refreshBtn = document.getElementById('game-restart-button');
+    this.input = document.getElementById('user-input');
+    this.refreshBtn.addEventListener('click', this.handleRefresh);
+    this.render();
+  }
+
+  render() {
+    this.isStart ? this.start() : this.finish();
+  }
+
+  setIsStart(bool) {
+    this.isStart = bool;
+    this.render();
+  }
+
+  start() {
+    this.form.addEventListener('submit', this.handleSubmit);
+    this.input.value = '';
+    this.input.focus();
+    this.computerInputNumbers = makeRandomNumber();
+    this.result.innerHTML = ``;
+    this.refreshBtn.style.display = 'none';
+  }
+
+  finish() {
+    this.form.removeEventListener('submit', this.handleSubmit);
+    this.result.innerHTML = `<h1>🎉성공했습니다🎉</h1><p>게임을 다시 시작하시겠습니까?</p>`;
+    this.refreshBtn.style.display = 'block';
+  }
+
+  handleRefresh = () => {
+    this.setIsStart(true);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const userInputNumbers = e.target[0].value;
+    this.validator(userInputNumbers) ? this.match(this.computerInputNumbers, userInputNumbers) : this.error();
+  };
+
+  match(computerInputNumbers, userInputNumbers) {
+    computerInputNumbers === userInputNumbers ? this.setIsStart(false) : this.fail(userInputNumbers);
+  }
+
+  validator(input) {
+    if (input.length > 3) {
+      return false;
+    }
+    let userInput = new Set(input + '');
+    return userInput.size === 3 ? true : false;
+  }
+
+  error() {
+    alert('중복되지 않는 세자리 수를 입력해주세요!');
+  }
+
+  fail() {
+    let message = '';
+    this.intersection(userInputNumbers) ? (message = this.intersection(userInputNumbers)) : (message = '낫씽');
+    this.result.textContent = message;
+  }
+
+  intersection(userInputNumbers) {
+    let ci = new Set(this.computerInputNumbers);
+    let ui = new Set(userInputNumbers);
+    let result = new Set();
+    let strike = 0;
+    let ball = 0;
+
+    for (const num of ui) {
+      if (ci.has(num)) result.add(num);
+    }
+
+    [...result].forEach((num) => {
+      [...ci].findIndex((value) => value === num) === [...ui].findIndex((value) => value === num) ? strike++ : ball++;
+    });
+
+    return strike && !ball
+      ? `${strike}스트라이크`
+      : ball && !strike
+      ? `${ball}볼`
+      : strike && ball
+      ? `${ball}볼 ${strike}스트라이크`
+      : false;
+  }
+}
+
+function makeRandomNumber() {
+  let result = new Set();
+  while (result.size !== 3) {
+    result.add(MissionUtils.Random.pickNumberInRange(1, 9));
+  }
+  console.log([...result].join(''));
+  return [...result].join('');
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-'use strict';
-
 // export default class BaseballGame {
 //     play(computerInputNumbers, userInputNumbers) {
 //       return "결과 값 String";
 //     }
 //   }
+
+import BaseballGame from './game.js';
 
 //   // 예시
 //   play(123, 456); // '낫싱'
@@ -15,102 +15,4 @@
 //   play(123, 134); // '1볼 1스트라이크'
 //   play(123, 132); // '2볼 1스트라이크'
 //   play(123, 124); // '2스트라이크
-
-let isStart = true;
-let computerInputNumbers;
-const form = document.querySelector('form');
-const result = document.getElementById('result');
-const refreshBtn = document.getElementById('game-restart-button');
-const input = document.getElementById('user-input');
-render();
-
-function render() {
-  isStart ? start() : finish();
-}
-
-function setIsStart(bool) {
-  isStart = bool;
-  render();
-}
-
-function start() {
-  input.textContent = '';
-  input.focus();
-  makeRandomNumber();
-  form.addEventListener('submit', handleSubmit);
-  result.innerHTML = ``;
-  refreshBtn.style.display = 'none';
-}
-
-function finish() {
-  form.removeEventListener('submit', handleSubmit);
-  result.innerHTML = `<h1>🎉성공했습니다🎉</h1><p>게임을 다시 시작하시겠습니까?</p>`;
-  refreshBtn.style.display = 'block';
-  refreshBtn.addEventListener('click', handleRefresh);
-}
-
-function handleRefresh() {
-  setIsStart(true);
-}
-
-function handleSubmit(e) {
-  e.preventDefault();
-  const userInputNumbers = e.target[0].value;
-  console.log('유저입력', userInputNumbers);
-  validator(userInputNumbers) ? match(computerInputNumbers, userInputNumbers) : error();
-}
-
-function makeRandomNumber() {
-  let result = new Set();
-  while (result.size !== 3) {
-    result.add(MissionUtils.Random.pickNumberInRange(1, 9));
-  }
-  computerInputNumbers = [...result].join('');
-  console.log(computerInputNumbers);
-}
-
-function match(computerInputNumbers, userInputNumbers) {
-  computerInputNumbers === userInputNumbers ? setIsStart(false) : fail(userInputNumbers);
-}
-
-function validator(input) {
-  if (input.length > 3) {
-    return false;
-  }
-  let userInput = new Set(input + '');
-  return userInput.size === 3 ? true : false;
-}
-
-function error() {
-  alert('중복되지 않는 세자리 수를 입력해주세요!');
-}
-
-function fail(userInputNumbers) {
-  let message = '';
-  intersection(userInputNumbers) ? (message = intersection(userInputNumbers)) : (message = '낫씽');
-  result.textContent = message;
-}
-
-function intersection(userInputNumbers) {
-  let ci = new Set(computerInputNumbers);
-  let ui = new Set(userInputNumbers);
-  let result = new Set();
-  let strike = 0;
-  let ball = 0;
-
-  for (const num of ui) {
-    if (ci.has(num)) result.add(num);
-  }
-
-  [...result].forEach((num) => {
-    [...ci].findIndex((value) => value === num) === [...ui].findIndex((value) => value === num) ? strike++ : ball++;
-  });
-
-  return strike && !ball
-    ? `${strike}스트라이크`
-    : ball && !strike
-    ? `${ball}볼`
-    : strike && ball
-    ? `${ball}볼 ${strike}스트라이크`
-    : false;
-}
+const game = new BaseballGame();
